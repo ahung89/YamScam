3fc650cf54b2b566c72f2bbd5a2493512465b520
Andrew Hung <druphoria@utexas.edu>
2017-08-06 12:16:57 -0700

    background baybay

---
+++ Assets/Scripts/UI/Background.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	void Awake()
+    {
+        GetComponent<Canvas>().worldCamera = Camera.main;
+    }
+}
