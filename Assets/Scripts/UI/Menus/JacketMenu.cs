388a3218f30d3aab54c7030ec3902882b84e9f36
Andrew W Hung <druphoria@utexas.edu>
2017-09-25 20:33:05 -0700

    started basic menu flow

---
+++ Assets/Scripts/UI/Menus/JacketMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class JacketMenu : MonoBehaviour {
+
+    public GameObject worldMapMenu;
+
+    public void TransitionToWorldMap()
+    {
+        MenuManager.Instance.PushPanel(worldMapMenu);
+    }
+
+    public void StartGame ()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void ExitGame ()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/Menus/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    public static MenuManager Instance;
+
+    public List<GameObject> nonStartingMenus;
+
+    private Stack<GameObject> panelStack;
+
+    private void Awake ()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance);
+        }
+
+        Instance = this;
+        panelStack = new Stack<GameObject>();
+        nonStartingMenus.ForEach(Destroy);
+    }
+
+    public void PushPanel (GameObject panelPrefab, bool zoom = false)
+    {
+        GameObject panel = Instantiate(panelPrefab, transform);
+
+        if (panelStack.Count > 0)
+        {
+            SetPanelEnabled(panelStack.Peek(), false);
+        }
+
+        panelStack.Push(panel);
+    }
+
+    public void PopPanel(bool zoom = false)
+    {
+        GameObject poppedPanel = panelStack.Pop();
+        Destroy(poppedPanel);
+
+        if (panelStack.Count > 0)
+        {
+            SetPanelEnabled(panelStack.Peek(), true);
+        }
+    }
+
+    void SetPanelEnabled(GameObject panel, bool enabled)
+    {
+        panel.GetComponent<CanvasGroup>().interactable = enabled;
+    }
+}
