ab2aaada9f52695d9cb14a0965102f6ba2bceb8b
rainply <rainply@gmail.com>
2017-09-29 01:38:29 -0700

    add world map scene.

---
+++ Assets/Scripts/UI/Menus/WorldMapManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WorldMapManager : MonoBehaviour {
+
+	public void LoadLevel(int levelId)
+	{
+		// load level if it is unlocked
+		if (levelId <= PlayerDataManager.LevelProgress) 
+		{
+			SceneManager.LoadSceneAsync(levelId);
+		}
+	}
+}
