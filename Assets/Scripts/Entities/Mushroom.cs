82bdb3bcf55e7fb1d220a18b9bb2f5f64f6872ff
Andrew W Hung <druphoria@utexas.edu>
2017-10-05 19:03:46 -0700

    draw arrows instead of lines

--- Assets/Scripts/Entities/Mushroom.cs
+++ Assets/Scripts/Entities/Mushroom.cs
@@ -6,11 +6,15 @@ using UnityEditor;
 public class Mushroom : MonoBehaviour {
     public GameObject target;
 
+    [Range(-1.5f, 1.5f)]
     public float height = 1.5f;
-    private static int numDashes = 30;
+    public float arrowLength = .1f;
+    public float arrowCountMultiplier = 5;
 
     private float adjustedHeight;
     private float trajectoryDuration;
+    private float xDistance;
+    private float a;
 
     private void OnTriggerEnter2D (Collider2D collider)
     {
@@ -35,7 +39,28 @@ public class Mushroom : MonoBehaviour {
 
         float velY = Mathf.Sqrt(-2 * Physics.gravity.y * adjustedHeight);
         float velX = displacementX / (Mathf.Sqrt(-2 * adjustedHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (displacementY - adjustedHeight) / Physics.gravity.y));
+
+        float timeToVertex = -velY / Physics.gravity.y;
+        xDistance = velX * timeToVertex;
+
+        a = (transformedPoint.y - adjustedHeight) / Mathf.Pow(transformedPoint.x - xDistance, 2);
+
         return new Vector2(velX, velY);
+    }
+
+    float CalculateHeight(float x)
+    {
+        return a * Mathf.Pow(x - xDistance, 2) + adjustedHeight;
+    }
+
+    float CalculateSlope(float x)
+    {
+        return 2 * a * (x - xDistance);
+    }
+
+    Vector2 RotateVector(Vector2 vec, float angle)
+    {
+        return new Vector2(vec.x * Mathf.Cos(angle) - vec.y * Mathf.Sin(angle), vec.x * Mathf.Sin(angle) + vec.y * Mathf.Cos(angle));
     }
 
     private void OnDrawGizmos ()
@@ -43,19 +68,31 @@ public class Mushroom : MonoBehaviour {
         bool selected = Selection.Contains(gameObject);
         Color col = selected ? Color.green : Color.grey;
         col.a = selected ? 1 : .5f;
+        Gizmos.color = col;
 
-        Vector2 launchVelocity = CalculateLaunchVelocity();
+        float sign = Mathf.Sign(target.transform.position.x - transform.position.x);
+
+        Vector2 launchVelocity = CalculateLaunchVelocity(); 
         Vector3 prevPoint = transform.position;
 
-        for (int i = 1; i < numDashes; i++)
+        int numArrows = (int)(arrowCountMultiplier * trajectoryDuration);
+        float increment = (target.transform.position.x - transform.position.x) / numArrows;
+
+        for (int i = 1; i < numArrows; i++)
         {
-            float t = i / (float)numDashes * trajectoryDuration;
-            Vector2 displacement = .5f * Physics.gravity.y * Vector2.up * t * t + launchVelocity * t;
-            Vector3 point = (Vector2)transform.position + displacement;
+            float x = increment * i;
+            float y = transform.position.y + CalculateHeight(increment * i);
+            float slope = CalculateSlope(x);
+            Vector2 tangent = sign * new Vector2(1, slope);
+
+            Vector2 point = new Vector2(transform.position.x + x, transform.position.y + CalculateHeight(x));
+            Vector2 angle1 = RotateVector(tangent, -20 * Mathf.Deg2Rad).normalized * arrowLength;
+            Vector2 angle2 = RotateVector(tangent, 20 * Mathf.Deg2Rad).normalized * arrowLength;
 
             if (i % 2 != 0)
             {
-                Debug.DrawLine(prevPoint, point, col);
+                Gizmos.DrawLine(point - angle1, point);
+                Gizmos.DrawLine(point - angle2, point);
             }
 
             prevPoint = point;
